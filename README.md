7683a252f24e0d13c1b970e0756b5d0c2c9f953f
adavila <andres.davila-corujo@us.af.mil>
2023-11-28 20:57:21 -0500

    Adding info
    Additions:
    - README.md
    - Comments for Clustering.js

--- README.md
+++ README.md
@@ -1,70 +1,55 @@
-# Getting Started with Create React App
+# Getting Started with Map App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project uses react-leaflet to create a map with clustering capabilities.
 
-## Available Scripts
+### Data
 
-In the project directory, you can run:
+The data displayed is described as:
+{
+"nodes": Map(id: int, {
+id: int,
+lon: float,
+lat: float,
+type: string,
+primary: boolean,
+name: string
+}),
+"links": [
+{"source": int, "target": int}
+]
+}
 
-### `npm start`
+The app contains two main components:
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+1. Controls - UI for controlling the markers rendered on the map
+2. MapView - Map to render markers
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+Assuming the data provided was yielded from a database search, the data returned from
+that search is called _first order_. All other data points related to those returned
+are called _second order_.
 
-### `npm test`
+Example: Given a list of data points where each one is an object with a sub/parent key
+containing a list of datapoints...
+{
+id: 1,
+type: 'parent',
+sub: [{
+id: 11,
+type: 'sub'
+}]
+}...the keys with the additional datapoints are referred as _second order_ while the original is _first order_. In the example above, the object with id 1 would be _first order_ while the object with id 11 would be _second order_.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+### Controls
 
-### `npm run build`
+Using the switches on the top-left, the user can select whether he/she wants to
+see the second order markers returned from the search and/or show the connections
+from first to second order markers.
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+The additional list of data points as buttons allows the user to select which data point
+to see on the map. If no data point is selected, only the first order markers are shown.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+### Interactivity
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+The Clustering component defines a handler for clicking each marker. If a marker is clicked the map will focus on that point and display its second order relationships with a line tracking the connection. This is useful in the event a user selects a single second order data point, only to see and select its first order data point which will display all second data points related. Think of it like going up the chain.
 
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+This is not a perfect implementation but most things aren't in their first iteration :).
--- src/components/mapping/Clustering.js
+++ src/components/mapping/Clustering.js
@@ -4,80 +4,97 @@ import "leaflet.markercluster/dist/leaflet.markercluster";
 import "leaflet.markercluster/dist/MarkerCluster.css";
 import "leaflet.markercluster/dist/MarkerCluster.Default.css";
 import { useMap } from "react-leaflet";
-import { marker, LatLng } from 'leaflet';
+import { marker, LatLng } from "leaflet";
 
-import { ParentIcon, ChildIcon } from '../icons/icons';
-import { ParentCluster, ChildCluster } from '../icons/clusters';
+import { ParentIcon, ChildIcon } from "../icons/icons";
+import { ParentCluster, ChildCluster } from "../icons/clusters";
 
-const Clustering = ({ searchResults, switches, setSwitches, selectedDataPoints, setSelectedDataPoints, markers }) => {
-  const map = useMap()
+/**
+ * Clustering: component defined under the react-leaflet's MapContainer context
+ * defining the map's interactivity
+ *
+ * @param {Dictionary} searchResults - adjacency list containing all data
+ * @param {Dictionary} switches - dictionary holding flags to control markers displayed on the map
+ * @param {()=>{}} setSwitches -state variable modifier
+ * @param {Array<Number>} selectedDataPoints - array of selected ids to display/inspect on the map
+ * @param {() => {}} setSelectedDataPoints - state variable modifier
+ * @param {Array<Dictionary>} markers - array of markers to render on the map
+ */
+const Clustering = ({
+  searchResults,
+  switches,
+  setSwitches,
+  selectedDataPoints,
+  setSelectedDataPoints,
+  markers,
+}) => {
+  const map = useMap();
 
-  const links = searchResults['links']
+  const links = searchResults["links"];
 
   const handleMarkerClick = (id) => {
-    const selectedTemp = []
+    const selectedTemp = [];
     // Verify if there exists relationship
-    const filteredSources = links.filter(link => link['source'] === id)
-    const filteredTargets = links.filter(link => link['target'] === id)
+    const filteredSources = links.filter((link) => link["source"] === id);
+    const filteredTargets = links.filter((link) => link["target"] === id);
 
     // Select all targets related to a source
-    filteredSources.forEach(value => {
-        selectedTemp.push(value['target'])
-    })
+    filteredSources.forEach((value) => {
+      selectedTemp.push(value["target"]);
+    });
     // Select all sources related to a target
-    filteredTargets.forEach(value => {
-        selectedTemp.push(value['source'])
-    })
-    selectedTemp.push(id)
-    setSelectedDataPoints([...selectedTemp])
+    filteredTargets.forEach((value) => {
+      selectedTemp.push(value["source"]);
+    });
+    selectedTemp.push(id);
+    setSelectedDataPoints([...selectedTemp]);
     setSwitches({
-        ...switches,
-        showRelationships: true,
-        showPolyLines: true
-    })
-  }
-  
-  useEffect(() => {
-    ParentCluster.clearLayers()
-    ChildCluster.clearLayers()
+      ...switches,
+      showRelationships: true,
+      showPolyLines: true,
+    });
+  };
 
-    const positions = []
+  useEffect(() => {
+    ParentCluster.clearLayers();
+    ChildCluster.clearLayers();
+
+    const positions = [];
 
     if (markers !== undefined) {
-        markers.forEach(({ position, id, type, popUpData}) => {
-            if (selectedDataPoints.length < 1 || selectedDataPoints.includes(id)) {
-                    marker(new LatLng(position.lat, position.lon),
-                        {icon: type === 'parent'
-                            ? ParentIcon
-                            : ChildIcon
-                        }
-                    )
-                        .addTo(
-                            type === 'parent'
-                            ? ParentCluster
-                            : ChildCluster
-                        )
-                        // .bindPopup(popUpData).openPopup()
-                        .bindTooltip(popUpData)
-                        .addEventListener('click', () => handleMarkerClick(id))
-                    positions.push([position.lat, position.lon])
-                }
-        })
+      markers.forEach(({ position, id, type, popUpData }) => {
+        if (selectedDataPoints.length < 1 || selectedDataPoints.includes(id)) {
+          // if no points selected render all data points
+          // if id selected render its marker on the map
+          marker(new LatLng(position.lat, position.lon), {
+            icon: type === "parent" ? ParentIcon : ChildIcon,
+          })
+            .addTo(type === "parent" ? ParentCluster : ChildCluster)
+            // .bindPopup(popUpData).openPopup()
+            .bindTooltip(popUpData)
+            .addEventListener("click", () => handleMarkerClick(id));
+          positions.push([position.lat, position.lon]);
+        }
+      });
 
-        // add the marker cluster group to the map
-        map.addLayer(ParentCluster)
-        map.addLayer(ChildCluster)
+      // add the marker cluster group to the map
+      map.addLayer(ParentCluster);
+      map.addLayer(ChildCluster);
 
-        const bounds = L.latLngBounds(
-            positions.length > 0
-            ? positions
-            : [[70,104], [10, 104]]
-        )
-        map.fitBounds(bounds, {paddingBottomRight: [100, -100]})
+      const bounds = L.latLngBounds(
+        positions.length > 0
+          ? positions
+          : [
+              [70, 104],
+              [10, 104],
+            ]
+      );
+      // Need to find a way to fit the map at different zoom levels
+      map.fitBounds(bounds, { paddingBottomRight: [100, -100] });
     }
-  }, [markers, map, selectedDataPoints])
+  }, [markers, map, selectedDataPoints]);
 
-  return null
-}
+  return null;
+};
 
 export default Clustering;
